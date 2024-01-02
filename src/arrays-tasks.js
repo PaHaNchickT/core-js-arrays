f7d591e207e0875466c8dc2e606ee0cf24aa1a21
Pavel <you@example.com>
2024-01-02 22:17:15 +0300

    feat: implement getAverage task (02.01.24 22:17)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -139,8 +139,14 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const out = arr.reduce(function unnamed(a, b) {
+    return a + b;
+  });
+  return +(out / arr.length).toFixed(2);
 }
 
 /**
