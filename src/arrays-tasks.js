0cf1908087874a00c3a6ef14a0bce0dc7f65d7a6
Pavel <you@example.com>
2024-01-03 06:06:28 +0300

    feat: implement getFalsyValuesCount task (03.01.24 06:06)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -478,8 +478,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const temp = arr.filter((e) => Boolean(e) === false);
+  return temp.length;
 }
 
 /**
