51eb05919c97a83c656f121533c57c2992b0d677
Pavel <you@example.com>
2024-01-03 03:12:54 +0300

    feat: implement createChunks task (03.01.24 03:12)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -409,8 +409,14 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const out = [];
+  out.push(arr.slice(0, chunkSize));
+  out.push(arr.slice(chunkSize, 2 * chunkSize));
+  out.push(arr.slice(2 * chunkSize, 3 * chunkSize));
+  out.push(arr.slice(3 * chunkSize, 4 * chunkSize));
+  out.push(arr.slice(4 * chunkSize, 5 * chunkSize));
+  return out.filter((e) => e.length > 0);
 }
 
 /**
