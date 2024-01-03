7317bee4d3a9b973a278a2451c45fdfe9649e243
Pavel <you@example.com>
2024-01-03 07:17:14 +0300

    feat: implement propagateItemsByPositionIndex task (03.01.24 07:17)

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -654,8 +654,34 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const out = [];
+  if (arr[0] !== undefined) {
+    out.push(arr[0]);
+  }
+  if (arr[1] !== undefined) {
+    out.push(arr[1]);
+    out.push(arr[1]);
+  }
+  if (arr[2] !== undefined) {
+    out.push(arr[2]);
+    out.push(arr[2]);
+    out.push(arr[2]);
+  }
+  if (arr[3] !== undefined) {
+    out.push(arr[3]);
+    out.push(arr[3]);
+    out.push(arr[3]);
+    out.push(arr[3]);
+  }
+  if (arr[4] !== undefined) {
+    out.push(arr[4]);
+    out.push(arr[4]);
+    out.push(arr[4]);
+    out.push(arr[4]);
+    out.push(arr[4]);
+  }
+  return out;
 }
 
 /**
